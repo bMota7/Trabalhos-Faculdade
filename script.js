5e8d163d588b87fb1be573e008364c21d5645c6b
Bruno Mota <117960184+bMota7@users.noreply.github.com>
2023-04-11 22:33:59 -0300

    Finalizado

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
+/* Calculadora */
 function calculateResistance() {
     const voltage = parseFloat(document.getElementById("voltage").value);
     const current = parseFloat(document.getElementById("current").value);
     const resistance = voltage / current;
     document.getElementById("resistance").value = resistance.toFixed(2);
-  }
-  
+}
+
+/* Botão reload */
+const button = document.querySelector(".btnReloadPage")
+button.addEventListener("click", () => {
+  window.location.reload()
+})
